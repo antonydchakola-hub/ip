db151ec02de06fcd11520ee49715960be962ef44
Antony Dennis Chakola <antonydchakola@gmail.com>
2026-08-19 18:26:46 +0800

    Add support for echo

--- src/main/java/Vector.java
+++ src/main/java/Vector.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Vector {
     public static void main(String[] args) {
         String banner = " __     _______ ____ _____ ___  ____\n"
@@ -5,13 +7,28 @@ public class Vector {
                 + "  \\ \\ / /|  _| | |     | || | | | |_) |\n"
                 + "   \\ V / | |___| |___  | || |_| |  _ <\n"
                 + "    \\_/  |______\\____| |_| \\___/|_| \\_\\\n";
-        String line = "____________________________________________________________";
+        String line = "    ____________________________________________________________";
+        
         System.out.println(line);
-        System.out.println(banner);
-        System.out.println(" Hello! I'm Vector");
-        System.out.println(" What can I do for you?");
+        System.out.print(banner);
+        System.out.println("     Hello! I'm Vector");
+        System.out.println("     What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            } else {
+                System.out.println(line);
+                System.out.println("     " + input);
+                System.out.println(line);
+            }
+        }
+        scanner.close();
     }
 }
