3c41f99f27ce8e2fc2fd0b440ce36ac7849e6079
Antony Dennis Chakola <antonydchakola@gmail.com>
2026-08-19 17:54:42 +0800

    Rename chatbot to Vector, update AGENTS.md, and update banner

---
+++ src/main/java/Vector.java
@@ -0,0 +1,10 @@
+public class Vector {
+    public static void main(String[] args) {
+        String banner = " __     _______ ____ _____ ___  ____\n"
+                + " \\ \\   / / ____/ ___|_   _/  _ \\|  _ \\\n"
+                + "  \\ \\ / /|  _| | |     | || | | | |_) |\n"
+                + "   \\ V / | |___| |___  | || |_| |  _ <\n"
+                + "    \\_/  |______\\____| |_| \\___/|_| \\_\\\n";
+        System.out.println(banner);
+    }
+}
