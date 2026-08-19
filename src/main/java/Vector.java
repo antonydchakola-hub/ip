c81bbd0ec7d5ed06677b8a745ee4a0dbb7b2b827
Antony Dennis Chakola <antonydchakola@gmail.com>
2026-08-19 18:16:04 +0800

    Implement Level-0: Greet and exit

--- src/main/java/Vector.java
+++ src/main/java/Vector.java
@@ -5,6 +5,13 @@ public class Vector {
                 + "  \\ \\ / /|  _| | |     | || | | | |_) |\n"
                 + "   \\ V / | |___| |___  | || |_| |  _ <\n"
                 + "    \\_/  |______\\____| |_| \\___/|_| \\_\\\n";
+        String line = "____________________________________________________________";
+        System.out.println(line);
         System.out.println(banner);
+        System.out.println(" Hello! I'm Vector");
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
