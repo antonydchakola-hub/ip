230a10630607587c811ee74b8481143d252c3591
Antony Dennis Chakola <antonydchakola@gmail.com>
2026-08-20 20:10:23 +0800

    Add Task class to represent tasks

---
+++ src/main/java/Task.java
@@ -0,0 +1,49 @@
+/**
+ * Represents a task with a description and a completion status.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Constructs a new Task with the given description.
+     * 
+     * @param description The description of the task.
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Gets the status icon of the task.
+     * 
+     * @return "X" if the task is done, " " otherwise.
+     */
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    /**
+     * Marks the task as done.
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Marks the task as not done.
+     */
+    public void unmarkAsDone() {
+        this.isDone = false;
+    }
+
+    /**
+     * Gets the description of the task.
+     * 
+     * @return The description.
+     */
+    public String getDescription() {
+        return this.description;
+    }
+}
