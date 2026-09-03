58a08585d4bd70724a1e5816e14aebe8d4046369
Antony <antonydchakola@gmail.com>
2026-09-04 00:21:04 +0800

    Add JavaFX UI controls and DialogBox for Level-10

---
+++ src/main/java/vector/DialogBox.java
@@ -0,0 +1,28 @@
+package vector;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+/**
+ * A custom control that represents a dialog box consisting of an ImageView to represent the speaker's face
+ * and a label containing text from the speaker.
+ */
+public class DialogBox extends HBox {
+    private Label text;
+    private ImageView displayPicture;
+
+    /**
+     * Constructs a DialogBox with the specified text and image.
+     *
+     * @param s The text to display.
+     * @param i The image representing the speaker.
+     */
+    public DialogBox(String s, Image i) {
+        text = new Label(s);
+        displayPicture = new ImageView(i);
+
+        this.getChildren().addAll(text, displayPicture);
+    }
+}
--- src/main/java/vector/Main.java
+++ src/main/java/vector/Main.java
@@ -2,7 +2,12 @@ package vector;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -10,11 +15,33 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-        stage.setScene(scene); // Setting the stage to show our scene
-        stage.show(); // Render the stage.
+        // Setting up required components
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+        stage.setScene(scene);
+        stage.show();
+
+        // Create sample dialog box
+        DialogBox dialogBox = new DialogBox("Hello!", userImage);
+        dialogContainer.getChildren().addAll(dialogBox);
     }
 }
